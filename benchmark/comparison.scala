071bc1f1c822b3bff87af53c26f5af0d6d22e99b
Michal Monselise <michal.monselise@gmail.com>
2021-07-01 15:49:56 -0400

    benchmark to graphs of snapshot

---
+++ benchmark/comparison.scala
@@ -0,0 +1,33 @@
+package org.temporalframes
+
+import java.util.Random
+
+import scala.reflect.runtime.universe.TypeTag
+
+import org.apache.spark.graphx.{Edge, Graph}
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions.{array, broadcast, col, count, explode, struct, udf, monotonically_increasing_id, expr}
+import org.apache.spark.sql.types._
+import org.apache.spark.storage.StorageLevel
+import org.graphframes._
+
+
+import org.graphframes.lib._
+import org.graphframes.pattern._
+
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark._
+import org.apache.spark.graphx._
+
+// To make some of the examples work we will also need RDD
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.graphx.{Graph, Edge, Pregel}
+import org.graphframes._
+import spark.implicits._
+
+import scala.collection.mutable._
+import scala.math._
